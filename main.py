4414b8e28daf7ba3c9e89400f05977c84ade5627
prajjawal05 <prajjawalagarwal@gmail.com>
2023-10-19 15:18:33 -0400

    added chunking, did not combine them yet

--- main.py
+++ main.py
@@ -1,46 +1,88 @@
 import ffmpeg
 import sys
+import os
 from datetime import datetime
-from enums import Resolution
+from constants import Resolution
+from config import resolution_scale
 
-types = ['1080p', '720p', '480p', '360p']
-scales = ['1920:1080', '1280:720', '858:480', '480:360']
 
-def transcode_into_type(filename, type, scale):
-    # changing the container format
-    output_file = 'output-{}.mp4'.format(type)
-    # changing some feature of codec
-    vf = 'scale={}'.format(scale)
-    return ffmpeg.input(filename).output(output_file, vcodec='libx264', acodec='aac', vf=vf).run_async(overwrite_output=True, quiet=True)
+def get_video_duration(filename):
+    probe = ffmpeg.probe(filename, v='error', select_streams='v:0', show_entries='stream=duration')
+    return float(probe['streams'][0]['duration'])
 
-#implement chunking
-#run this in openwhisk custom container
-def transcode(filename):
-    all_processes = []
-    for i in range(0, len(scales)):
-        type = types[i]
-        scale = scales[i]
-        all_processes.append(transcode_into_type(filename, type, scale))
-    
+
+def transcode_into_type(filename, resolution_format):
+    output_directory = "transcoded_chunks"
+    output_filename = filename.split("/")[1]
+    output_file = '{}/{}'.format(output_directory, output_filename)
+    os.makedirs(output_directory, exist_ok=True)
+    vf = 'scale={}'.format(resolution_scale[resolution_format.name])
+    process = ffmpeg.input(filename).output(output_file, vcodec='libx264', acodec='aac', vf=vf).run_async(overwrite_output=True, quiet=True)
+    return output_file, process
+
+
+def transcode(filenames, resolution_format):
+    batch_size = 5
+    transcoded_files = []
     start = datetime.now()
 
-    for process in all_processes:
-        process.wait()
+    for i in range(0, len(filenames), batch_size):
+        print("Processing batch - {}".format(i//5 + 1))
+        current_batch = []
+        for j in range(i, i+batch_size):
+            if j >= len(filenames):
+                break
+            output_file, transcode_process  = transcode_into_type(filenames[j], resolution_format)
+            current_batch.append(transcode_process)
+            transcoded_files.append(output_file)
+        
+        for process in current_batch:
+            process.wait()
 
     end = datetime.now()
 
-    print('Completed in {}'.format(end-start))
+    print('Processed all batches in {}'.format(end-start))
+
+    return transcoded_files
+
+
+def split(filename):
+    print('Starting to chunk')
+
+    output_directory = "output_chunks"
+    chunk_size = 10
+    duration = get_video_duration(filename)
+    splits = []
+    num_chunks = int(duration / chunk_size)
+    os.makedirs(output_directory, exist_ok=True)
+
+    start = datetime.now()
     
+    for i in range(num_chunks):
+        output_file = f"{output_directory}/chunk_{i}.mp4"
+        ffmpeg.input(filename, ss=i * chunk_size, t=chunk_size).output(output_file, codec='copy').run(overwrite_output=True, quiet=True)
+        splits.append(output_file)
+    
+    end = datetime.now()
+
+    print('Completed chunking in {}'.format(end-start))
+
+    return splits
+
+
 if __name__ == '__main__':
     args = sys.argv[1:]
+    resolution_format = Resolution._360p
+
     if len(args) == 2 and args[0] == '-format':
         try:
-            format = Resolution(args[1])
+            resolution_format = Resolution(args[1])
         except:
             print("Resolution does not exists. Using the default")
-            format = Resolution._360p
-        
-    print(format)
+
+    splits = split('facebook.mp4')
+    transcoded_chunks = transcode(splits, resolution_format)
+    print(transcoded_chunks)
 
     # transcode('facebook.mp4')
 
