381ef7545e571c21ce0131d4f5e1daf8df5952b6
prajjawal05 <prajjawalagarwal@gmail.com>
2023-11-19 03:14:17 -0500

    separated out main orchestrator, did not test it

---
+++ BaseOrchestrator.py
@@ -0,0 +1,67 @@
+import requests
+import urllib3
+from time import sleep
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+class BaseOrchestrator:
+    def __init__(self, auth) -> None:
+        self.auth = auth
+        self.url = "https://localhost:31001/api/v1/namespaces"
+
+    def __extract_activation_ids(act_dict):
+        return list(map(lambda x: x['activationId'], act_dict))
+
+    def __get_call(self, api_url):
+        response = requests.get(api_url, auth=self.auth, verify=False)
+        return response.json()
+
+    def __post_call(self, api_url, params):
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(
+            api_url, headers=headers, auth=self.auth, verify=False, json=params)
+        return response.json()
+
+    def __poller(self, activation_ids):
+        print("Polling for: {}".format(activation_ids))
+        output = [None]*len(activation_ids)
+
+        def _get_url(activation_id):
+            return "{}/guest/activations/{}".format(self.url, activation_id)
+
+        while (list(filter(lambda o: o is None, output))):
+            sleep(2)
+            for index, act_id in enumerate(activation_ids):
+                if output[index] is not None:
+                    continue
+                url = _get_url(activation_id=act_id)
+
+                result = self.__get_call(url)
+                if result.get('end', None) is not None:
+                    print("Poll completed for: {}".format(act_id))
+                    output[index] = result
+
+        return output
+
+    def prepare_action(self, name, params):
+        return {
+            'name': name,
+            'body': params,
+        }
+
+    def make_action(self, actions):
+        activation_ids = []
+
+        def _get_url(action_name):
+            return "{}/guest/actions/{}".format(self.url, action_name)
+
+        for action in actions:
+            action_response = self.__post_call(
+                _get_url(action['name']), action['body'])
+            activation_ids.extend(
+                self.__extract_activation_ids(action_response))
+
+        results = self.__poller(activation_ids)
+
+        return results
--- asyncorchestrator.py
+++ asyncorchestrator.py
@@ -1,56 +1,17 @@
-import asyncio
-import requests
-import urllib3
-from time import sleep
+from BaseOrchestrator import BaseOrchestrator
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 auth = ("23bc46b1-71f6-4ed5-8c54-816aa4f8c502",
         "123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP")
+orch = BaseOrchestrator(auth)
 
 
-def extract_activation_ids(act_dict):
-    return list(map(lambda x: x['activationId'], act_dict))
+action_name = 'transcoder'
 
 
-async def invoke_url_async(api_url, params):
-    headers = {"Content-Type": "application/json"}
-    response = await asyncio.to_thread(requests.post, api_url, headers=headers, auth=auth, verify=False, json=params)
-    return response.json()
-
-
-def invoke_url(api_url):
-    response = requests.get(api_url, auth=auth, verify=False)
-    return response.json()
-
-
-def poller(activation_ids):
-    print("Polling for: {}".format(activation_ids))
-    output = [None]*len(activation_ids)
-
-    def _get_url(activation_id):
-        return "https://localhost:31001/api/v1/namespaces/guest/activations/{}".format(activation_id)
-
-    while (list(filter(lambda o: o is None, output))):
-        sleep(2)
-        for index, act_id in enumerate(activation_ids):
-            if output[index] is not None:
-                continue
-            url = _get_url(activation_id=act_id)
-
-            result = invoke_url(url)
-            if result.get('end', None) is not None:
-                print("Poll completed for: {}".format(act_id))
-                output[index] = result
-
-    return output
-
-
-async def main():
+def main():
     num_chunks = 5
     transcoding_parallelisation = 2
-
-    url = "https://localhost:31001/api/v1/namespaces/guest/actions/transcoder"
 
     print("\n** Chunking **")
     params = {
@@ -58,9 +19,8 @@ async def main():
         "num_chunks": num_chunks,
         "input": "facebook.mp4"
     }
-    split_action = invoke_url_async(url, params)
-    splt_activation = extract_activation_ids(await asyncio.gather(split_action))
-    split_results = poller(splt_activation)
+    split_action = orch.prepare_action(action_name, params)
+    split_results = orch.make_action([split_action])
     chunks = split_results[0]['response']['result']['body']['splits']
 
     print(f"\n** Transcoding in batches of: {transcoding_parallelisation} **")
@@ -77,20 +37,18 @@ async def main():
                 "input": chunks[j],
                 "resolution": "360p"
             }
-            transcoding_action = invoke_url_async(url, params)
-            transcoding_actions.append(transcoding_action)
+            transcoding_actions.append(
+                orch.prepare_action(action_name, params))
 
-        transcoding_activations = extract_activation_ids(await asyncio.gather(*transcoding_actions))
-        poller(transcoding_activations)
+        orch.make_action(transcoding_actions)
 
     print("\n** Combining **")
     params = {
         "type": "combine",
         "input": chunks
     }
-    combine_action = invoke_url_async(url, params)
-    combine_activation = extract_activation_ids(await asyncio.gather(combine_action))
-    combine_results = poller(combine_activation)
+    combine_action = orch.prepare_action(action_name, params)
+    combine_results = orch.make_action([combine_action])
 
     print("\n** Done **")
     print("Output available at: {}".format(
@@ -98,7 +56,7 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
     # poller(['22b0335cebae4d4fb0335cebaefd4fff'])
 
 
