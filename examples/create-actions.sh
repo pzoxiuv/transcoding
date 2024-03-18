0d2bfbf7035b0a9cc387b86e45821ca94826d656
alex <mmerenstein@cs.stonybrook.edu>
2024-03-17 22:36:25 -0400

    Update action creation script

--- examples/create-actions.sh
+++ examples/create-actions.sh
@@ -6,29 +6,9 @@ echo $IMAGE_NAME
 for a in "$@"; do
     action_name=$(basename $(dirname $a))
     package_name=$(basename $(dirname $(dirname $a)))
-    echo $package_name/$action_name
+
+    zip_name=${package_name}_${action_name}.zip
+    zip -r ${zip_name} $(dirname $a)/*
+    wsk action create ${action_name} ${zip_name} --docker ${image_name} --insecure
+    rm ${zip_name}
 done
-
-exit
-
-# Copy transcodingActions.py and constants.py to the temporary directory
-for action in `find ${action_base_dir}/actions/ -mindepth 1 -type d`; do 
-    zip -r ${action}.zip ${action}/*
-done
-cp -r $(action_dir)/* $
-cp transcodingActions.py deployment/__main__.py
-cp constants.py deployment/
-
-# Zip the contents of the temporary directory
-cd deployment
-zip -r action.zip *
-
-# Move the zip file back to the original directory if needed
-mv action.zip ../
-
-# Clean up the temporary directory
-cd ..
-rm action.zip
-rm -rf deployment
-
-wsk action create ${action_name} action.zip --docker ${image_name} --insecure
